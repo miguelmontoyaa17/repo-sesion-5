2dadc79478e01406ddf4ac9efff4cf0d31838c74
miguelmontoyaa17 <miguelmontoyaa17@gmail.com>
2026-05-28 16:42:19 -0500

    feat: agrego saludo.py con datos del aprendiz

---
+++ saludo.py
@@ -0,0 +1,7 @@
+# saludo.py
+nombre = 'Miguel Castro'
+carrera = 'Análisis y Desarrollo de Software'
+proyecto = 'LocalUp'
+print(f'Hola, soy {nombre}')
+print(f'Estudio {carrera} en el SENA')
+print(f'Mi proyecto es: {proyecto}')
