f745a8fc5e1ad2b61c5578c1b63f0fa60dcea841
miguelmontoyaa17 <miguelmontoyaa17@gmail.com>
2026-05-28 16:46:05 -0500

    feat: agrego calculadora.py con operaciones básicas

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+# calculadora.py
+num1 = 0 # cambiá este valor
+num2 = 0 # cambiá este valor
+print(f'Suma: {num1} + {num2} = {num1 + num2}')
+print(f'Resta: {num1} - {num2} = {num1 - num2}')
+print(f'Multiplicación: {num1} x {num2} = {num1 * num2}')
+# Agregá acá una operación más — división, módulo o potencia
+print(f'division: {num1} / {num2} = {num1 / num2}')
