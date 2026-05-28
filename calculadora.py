7e6f59ae9f2168c694dfe1ec27ada3a7449fdb9b
miguelmontoyaa17 <miguelmontoyaa17@gmail.com>
2026-05-28 16:54:05 -0500

    feat: agrego división segura a la calculadora

--- calculadora.py
+++ calculadora.py
@@ -6,3 +6,8 @@ print(f'Resta: {num1} - {num2} = {num1 - num2}')
 print(f'Multiplicación: {num1} x {num2} = {num1 * num2}')
 # Agregá acá una operación más — división, módulo o potencia
 print(f'division: {num1} / {num2} = {num1 / num2}')
+
+if num2 != 0:
+    print(f'División: {num1} / {num2} = {num1 / num2}')
+else:
+    print('División: no se puede dividir por cero')
